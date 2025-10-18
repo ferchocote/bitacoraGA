8d714ac487a0f4da92bb23c5218d8ec08b58268c
netcote1 <netcote1@hotmail.com>
2025-10-18 17:42:02 -0500

    Ajuste de ocultar los documentos en la pestaña de transporte para el perlfil de cliente

--- bitacora_detalle_filtro.php
+++ bitacora_detalle_filtro.php
@@ -324,12 +324,13 @@ if (!isset($id)) {
                                 </svg>
                             </a>
                             <?php endif; ?>
+                            <?php if ($usuario->rol_codigo !== 'CLI') : ?>
                              <a href="javascript:void(0);" class="documentos-entrada" data-entrada='${JSON.stringify(item).replace(/'/g, "&apos;")}' style="color: #2cd857ff; text-decoration: none" title="Gestionar Documentos">
                                 <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                   <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8.828A2 2 0 0 0 19.414 7.414l-4.828-4.828A2 2 0 0 0 12.172 2H6zm6 1.414L18.586 10H13a1 1 0 0 1-1-1V3.414zM6 4h5v5a3 3 0 0 0 3 3h5v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V4z" clip-rule="evenodd"/>
                                 </svg>
                             </a>
-                            
+                            <?php endif; ?>
                             </td>
                         </tr>
                     `).join('')}
